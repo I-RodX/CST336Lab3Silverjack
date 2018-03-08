13a626c4e9e73aebf67721ee35394c61d958e955
Isaac Rodriguez <isarodriguez@csumb.edu>
2018-03-08 00:25:51 +0000

    started developing setup function

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,7 @@
 <?php
-    $suits=array("daimonds","clubs","hearts", "spades");
-    $cardValues=array("1","2","3","4","5","6","7","8","9","10","11","12","13");
+   
     $deck = array();
-    foreach($suits as $suit){
-        foreach($cardValues as $cardValue){
-            $deck[] = array("cardValue"=>$cardValue,"suit"=>$suit);
-        }
-    }
+  
    
     function shuffleDeck(){
         global $deck;
@@ -16,8 +11,21 @@
     function drawCard(){
         global $deck;
         $card = array_shift($deck);
+        echo $card['cardValue']." of ". $card['suit'];
+    }
+    
+    function setup(){
+        global $deck;
+        $suits=array("daimonds","clubs","hearts", "spades");
+        $cardValues=array("1","2","3","4","5","6","7","8","9","10","11","12","13");
+         
+        foreach($suits as $suit){
+            foreach($cardValues as $cardValue){
+                $deck[] = array("cardValue"=>$cardValue,"suit"=>$suit);
+            }
+        }
         
-        return $card;
+        shuffleDeck($deck);
     }
     
 ?>
